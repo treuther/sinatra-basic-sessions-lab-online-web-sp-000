42189617c5e366bba1eb5a57bd86fbffbad0abf3
Tracy Reuther <tracyreuther@gmail.com>
2019-09-13 18:27:12 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,6 +2,11 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   
+  configure do
+    enable: sessions
+    set :session_secret, "secret"
+  end
+  
   get '/hey' do
     @session = session
   end
